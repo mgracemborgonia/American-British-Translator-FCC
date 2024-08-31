a3c4c82e4ee721e2b331eb92ffdc19ec44eb4215
Mary Grace Borgonia <mgrace.borgonia@gmail.com>
2024-08-31 18:15:22 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,32 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+        let text = req.body.text;
+        let locale = req.body.locale;
+        let translation = "";
+        if(locale === undefined || text === undefined){
+          res.json({error: 'Required field(s) missing'});
+          return;
+        }
+        
+        if(!text){
+          res.json({error: 'No text to translate'});
+          return;
+        } 
+        
+        if(locale === "american-to-british"){
+          translation = translator.translateToBritEng(text);         
+        }else if(locale === "british-to-american"){
+          translation = translator.translateToAmerEng(text);  
+        }else{
+          res.json({error: 'Invalid value for locale field'});
+          return;
+        }
+  
+        if(text === translation){
+          translation = "Everything looks good to me!"; 
+        }
+        return res.json({text: text, translation: translation}); 
+      });
     });
 };
